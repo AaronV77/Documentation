ccb667a58a1ef030ad5362bc20a1caeef26baa96
AaronV77 <aaronvaloroso77@gmail.com>
2017-08-30 21:13:19 -0500

    New CentOS script update

--- centosVBoxSetup.sh
+++ centosVBoxSetup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-read -p "Please Insert Guest Additions, under the Devices. (Just hit enter): " $random
+read -p "Please be root already when running this script. (Just hit enter): " $random
+read -p "Please Insert Guest Additions, under the Devices. (Just hit enter): " $random2
 
 cd ~
-sudo passwd
-su
-yum update
-yum update kernel*
+sudo yum update
+sudo yum update kernel*
 mkdir /media/VirtualBoxGuestAdditions
 mount -r /dev/cdrom /media/VirtualBoxGuestAdditions
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install gcc kernel-devel kernel-headers dkms make bzip2 perl
+sudo yum install gcc kernel-devel kernel-headers dkms make bzip2 perl
 KERN_DIR=/usr/src/kernels/`uname -r`
 export KERN_DIR
 cd /media/VirtualBoxGuestAdditions
