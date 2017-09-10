609aadb1d82ecd5fb733a06c72642cd359fd9d82
AaronV77 <aaronvaloroso77@gmail.com>
2017-09-10 13:34:11 -0500

    Lustre scripts update

---
+++ scripts/Lustre_Scripts/lustre-server-P2.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+
+else 
+
+read -p "Is this an ost server? (y/n): " ost
+
+if [ "$ost" == "y" | "$ost" == "Y" ]
+then
+
+read -p "MGS node IP address: " address
+ifup enp0s8
+modprobe lnet
+lctl network up
+lctl list_nids
+mkfs.lustre --ost --fsname=temp --mgsnode=$address@tcp0 --index=0 /dev/sdb
+mkdir /mnt/ost
+mount -t lustre /dev/sdb /mnt/ost
+mount
+reset
+df -h
+mv lustre-server-P2.sh /root
+
+else if [ "$ost" == "n" | "$ost" == "N" ]
+
+ifup enp0s8
+modprobe lnet
+lctl network up
+lctl list_nids
+mkfs.lustre --fsname=temp --mgs --mdt --index=0 /dev/sdb
+mkdir /mnt/mdt
+mount -t lustre /dev/sdb /mnt/ost
+mount
+reset
+df -h
+mv lustre-server-P2.sh /root
+
+else 
+
+echo " Sorry didn't understand the input." 
+
+fi
